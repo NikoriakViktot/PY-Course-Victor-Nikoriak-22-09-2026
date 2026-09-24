1d9e8e7e8ff062aaee3f8b72ef265f0cdedbc3f2
AlinaRadchenko <alinaradcenko175@gmail.com>
2026-09-24 13:25:02 +0300

    Complete homework 01

---
+++ fix_me.py
@@ -0,0 +1,9 @@
+
+#SyntaxError: unterminated string literal (detected at line 1)
+print("My name is Anna")
+#SyntaxError: '(' was never closed
+print("I am learning Python")
+#SyntaxError: '(' was never closed
+print("Goodbye")
+
+#AI log: використовувала для пояснення помилок
---
+++ task5.py
@@ -0,0 +1,2 @@
+print("Alina", "Kharkiv", "Хочу створювати власні проєкти\n", sep=" | ", end="===")
+#AI log: none
