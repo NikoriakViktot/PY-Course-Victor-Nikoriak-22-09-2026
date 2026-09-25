141daa0a4dd7293aefb87d71f629c49c0a40f697
Medimost <sunswanleva@gmail.com>
2026-09-25 13:13:20 +0200

    Complete homework 01

---
+++ assignments/fix_me.py
@@ -0,0 +1,7 @@
+#Missing closing quote ["],Unresolved reference 'Print'
+print("My name is Anna")
+#Unresolved reference 'Print'
+print("I am learning Python")
+#',' or ')' expected
+print("Goodbye")
+# AI log: не використовувала
