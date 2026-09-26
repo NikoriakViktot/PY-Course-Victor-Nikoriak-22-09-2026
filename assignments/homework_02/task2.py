cda5688347352453881482097c08c503a7b24886
telfyy <tsukanovartem4@gmail.com>
2026-09-26 21:02:16 +0300

    Complete Homework 02

---
+++ assignments/homework_02/task2.py
@@ -0,0 +1,15 @@
+# AI log: none
+
+# Skoda Audi Bmw Ford
+print('Skoda', 'Audi', 'Bmw', 'Ford')
+
+# Skoda, Audi, Bmw, Ford
+print('Skoda', 'Audi', 'Bmw', 'Ford', sep=', ')
+
+# Skoda, Audi, Bmw, Ford | car brands
+print('Skoda', 'Audi', 'Bmw', 'Ford', sep=', ', end=' | car brands\n')
+
+# one, two...
+print('one', 'two', sep=', ', end='...\n')
+
+print('one', '\ttwo', sep=', ', end='...')
